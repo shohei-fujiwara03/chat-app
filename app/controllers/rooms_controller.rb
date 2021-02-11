62495468537de88198430d2fc8d6c7c514dd68d4
shohei-fujiwara03 <nuenuewara@gmail.com>
2021-02-11 20:15:13 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,4 +3,21 @@ class RoomsController < ApplicationController
   def new
   @room = Room.new
   end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+    # Railsではセキュリティ対策として、保存する前にストロングパラメーターを使い、許可するパラメーターを指定してから、データを保存するよう推奨されている。
+  end
+
 end
